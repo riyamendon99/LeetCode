15a1ecccc24c06f3baa9aaf2fbe4684bc7774495
Riya Mendon <61388675+riyamendon99@users.noreply.github.com>
2024-03-26 10:04:34 -0700

    Time: 65 ms (55.25%), Space: 17.4 MB (37.39%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -6,19 +6,12 @@ class Solution:
         count = 0
         cost = 0
         while(right<len(s)):
-            print("Left ", left)
-            print("Right ", right)
             if s[right] != t[right]:
                 cost = cost + abs(ord(s[right])-ord(t[right]))
-                print("Cost ", cost)
                 while cost>maxCost:
                     cost = cost - abs(ord(s[left])-ord(t[left]))
-                    print("Cost Now ", cost)
                     left = left + 1
-                    print("Left Now ", left)
-                    print("Right Now ", right)
             result = max(result, right-left+1)
-            print("Result ", result)
             right = right + 1
         return result
         
