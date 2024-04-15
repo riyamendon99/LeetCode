70ef44837a855214bead0b569fa7e650fc3fba99
Riya Mendon <61388675+riyamendon99@users.noreply.github.com>
2024-04-15 09:12:59 -0700

    Time: 157 ms (19.51%), Space: 17.3 MB (47.04%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -5,20 +5,20 @@ class SmallestInfiniteSet:
         for i in range(1,1001):
             self.arr.append(i)
         heapq.heapify(self.arr)
-        #self.result = []
         
 
     def popSmallest(self) -> int:
         ele = heapq.heappop(self.arr)
+        """No need to do the below it automatically updates the list"""
         #self.arr.remove(ele)
         #print(ele)
-        #self.result.append(ele)
         return ele
         
         
 
     def addBack(self, num: int) -> None:
         if num not in self.arr:
+            """No need to do the below it automatically updates the list"""
             #self.arr.append(num)
             heapq.heappush(self.arr, num)
             
