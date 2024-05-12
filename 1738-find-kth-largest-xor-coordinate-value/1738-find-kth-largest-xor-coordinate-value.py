1ca3ed2c48d342c6e65c4144fb69547bbe84e3b2
Riya Mendon <61388675+riyamendon99@users.noreply.github.com>
2024-05-12 13:05:39 -0700

    Time: 4990 ms (5.00%), Space: 232.3 MB (5.00%) - LeetHub

--- 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.py
+++ 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.py
@@ -1,10 +1,18 @@
 class Solution:
     
     def kthLargestValue(self, matrix: List[List[int]], k: int) -> int:
-        
+        """Took 2 hours"""
+        #Ratta this formula
         def getXOR(x, y):
             return (x | y) & (~x | ~y)
-        
+        """For this type of question where they have asked the below:
+
+The value of coordinate (a, b) of the matrix is the XOR of all matrix[i][j] where 0 <= i <= a < m and 0 <= j <= b < n (0-indexed).
+
+
+brute force will make it super complex and lead to time limit exception. Use hashMap to store repetitive. ALSO VVVIMP to know which values are being repeated. Take the basic calculation and try to come up with how you can reduce the complexity by reusing values and not calculating them every time.
+
+I first thought that the matrix values need to be stored. But later by trial and error found out that the XOR values need to be stored."""
         heap = []
         hashMap = {}
         value = 0
@@ -18,11 +26,8 @@ class Solution:
                 if j-1>=0 and i-1>=0:
                     value = getXOR(value, hashMap[(i-1,j-1)])
                 hashMap[(i,j)] = value
-                heappush(heap,value)
-                
-                    
+                heappush(heap,value)          
         result = heapq.nlargest(k, heap)
-        print(result)
         return result[k-1]
                         
                         
